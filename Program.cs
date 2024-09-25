5d0dddd6a0ee18dc305ce6dd017a5c5ff7881ec5
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-25 10:45:18 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Newtonsoft.Json;
+using SerializaçãoJSON.Models;
+
+//Istanciando um Objeto
+Venda v1 = new Venda(1, "Material de escritório", 25.00M);
+
+//Conventendo este objeto em JSON
+string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+
+//Escrevendo o JSON em um Arquivo
+File.WriteAllText("Arquivos/vendas.json", serializado);
+
+//Imprimindo o JSON
+Console.WriteLine(serializado);
