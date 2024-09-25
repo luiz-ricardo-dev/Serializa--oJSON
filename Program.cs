38e4f594fe72dacc90a8ba28df6663cd3009fa4a
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-25 11:47:24 -0400

    Serializando uma coleção de objetos

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 using Newtonsoft.Json;
 using SerializaçãoJSON.Models;
 
+DateTime dataAtual = DateTime.Now;
+
+//Criando Uma Lista
+List<Venda> listaVendas =  new List<Venda>();   
+
 //Istanciando um Objeto
-Venda v1 = new Venda(1, "Material de escritório", 25.00M);
+Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+Venda v3 = new Venda(3, "Hardware", 250.00M, dataAtual);
+
+//Adicionando os objetos na lista
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+listaVendas.Add(v3);
 
 //Conventendo este objeto em JSON
-string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+//string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+//Serializando uma Colection 
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
 //Escrevendo o JSON em um Arquivo
 File.WriteAllText("Arquivos/vendas.json", serializado);
--- obj/Debug/net8.0/SerializaçãoJSON.AssemblyInfo.cs
+++ obj/Debug/net8.0/SerializaçãoJSON.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SerializaçãoJSON")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0dddd6a0ee18dc305ce6dd017a5c5ff7881ec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SerializaçãoJSON")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SerializaçãoJSON")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
